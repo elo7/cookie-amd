4c08a115384e4c3fb7543209fcbe10cb2ea9ac03
William Seiti Mizuta <login+williammizuta@elo7.com>
2015-06-19 10:59:15 -0300

    Initial commit

---
+++ cookie.js
@@ -0,0 +1,20 @@
+define('cookie', [], function() {
+	return {
+		'set' : function(name, value, days) {
+			var d = new Date();
+		    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+		    var expires = "expires=" + d.toUTCString();
+		    document.cookie = name + "=" + value + "; " + expires + ";path=/";
+		},
+		'get' : function(name) {
+			var cname = name + "=";
+		    var ca = document.cookie.split(';');
+		    for(var i=0; i<ca.length; i++) {
+		        var c = ca[i];
+		        while (c.charAt(0) == ' ') c = c.substring(1);
+		        if (c.indexOf(cname) != -1) return c.substring(cname.length,c.length);
+		    }
+		    return "";
+		}
+	};
+});
