2a44d1248fecf46ce771d648d936c89f36824709
Luiz Corte Real <login+luiz@elo7.com>
2020-03-09 10:36:45 -0300

    [better-dev] Add terser and eslint for better developer environment

--- test/cookieTest.js
+++ test/cookieTest.js
@@ -1,30 +1,32 @@
+'use strict';
+
 var fs = require('fs');
 var vm = require('vm');
-var assert = require("assert");
+var assert = require('assert');
 
 vm.runInThisContext(fs.readFileSync('node_modules/define-async/async-define.js'));
 vm.runInThisContext(fs.readFileSync('cookie.js'));
 
-document = {
-	'cookie': ""
+global.document = {
+	'cookie': '',
 };
 
 define(['cookie'], function(cookie) {
-	describe('Cookie', function(){
-		it("should set a cookie", function() {
-			cookie.set('cookie-test','cookie-value',1);
+	describe('Cookie', function() {
+		it('should set a cookie', function() {
+			cookie.set('cookie-test', 'cookie-value', 1);
 			var ca = document.cookie.split(';');
-			assert.equal(ca[0].split("=")[0], "cookie-test");
-			assert.equal(ca[0].split("=")[1], "cookie-value");
+			assert.equal(ca[0].split('=')[0], 'cookie-test');
+			assert.equal(ca[0].split('=')[1], 'cookie-value');
 		});
 
-		it("should get a cookie", function() {
-			cookie.set('cookie-test','cookie-value',1);
-			assert.equal(cookie.get("cookie-test"), "cookie-value");
+		it('should get a cookie', function() {
+			cookie.set('cookie-test', 'cookie-value', 1);
+			assert.equal(cookie.get('cookie-test'), 'cookie-value');
 		});
 
-		it("should return null if cookie dont exist", function() {
-			assert.equal(cookie.get("cookie-test-que-nao-existe"), "");
+		it('should return null if cookie dont exist', function() {
+			assert.equal(cookie.get('cookie-test-que-nao-existe'), '');
 		});
 	});
 });
